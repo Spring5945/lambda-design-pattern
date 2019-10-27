feaa81f31283ded60c740a78c0547719150aa032
zhengyu.nie <18701991304@163.com>
2019-10-27 17:34:09 +0800

    feat(func):add function implements

---
+++ lambda-design-pattern/src/main/java/com/zhengyu/lambda/function/FunctionTest.java
@@ -0,0 +1,33 @@
+package com.zhengyu.lambda.function;
+
+import com.google.common.base.Preconditions;
+
+import java.util.function.Function;
+
+/**
+ * @author zhengyu.nie
+ * 2019.10.27
+ */
+public class FunctionTest {
+    public static void main(String[] args) {
+
+        Preconditions.checkArgument(
+                "HELLOWORLD10".equals(
+                        appendFunc().andThen(lenthAppendFunc()).andThen(toUpperCaseFunc())
+                                .apply("hello")
+                ), "function is running bad");
+
+    }
+
+    private static Function<String, String> appendFunc() {
+        return s -> s + "world";
+    }
+
+    private static Function<String, String> lenthAppendFunc() {
+        return s -> s + s.length();
+    }
+
+    private static Function<String, String> toUpperCaseFunc() {
+        return s -> s.toUpperCase();
+    }
+}
